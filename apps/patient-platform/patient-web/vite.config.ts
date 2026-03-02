e0dd00cfd1d3ec502fc9084adc63024ee1891eb8
Jaydip-sketch <jaydip.p@inheritx.com>
2026-03-02 18:50:57 +0530

    Revert "patient-web: add allowed host oncolife-patient.inheritxdev.in, add serve script for dist"
    This reverts commit e0e16186eb8608dfde967d7036a01f669b9073cd.

--- apps/patient-platform/patient-web/vite.config.ts
+++ apps/patient-platform/patient-web/vite.config.ts
@@ -17,9 +17,6 @@ export default defineConfig(({ mode }) => {
       host: true,
       port: 5173,
       cors: true,
-      allowedHosts: [
-        'oncolife-patient.inheritxdev.in',
-      ],
       proxy: {
         '/api': {
           target: env.VITE_GATEWAY_URL || 'http://localhost:8000',
@@ -33,13 +30,6 @@ export default defineConfig(({ mode }) => {
           secure: false,
         }
       }
-    },
-    preview: {
-      port: 5173,
-      strictPort: true,
-      allowedHosts: [
-        'oncolife-patient.inheritxdev.in',
-      ],
     },
     define: {
       global: 'globalThis',
