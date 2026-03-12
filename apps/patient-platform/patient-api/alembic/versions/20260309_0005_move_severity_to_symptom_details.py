85956bd9e165116121876e5cc66996b54da5eb1d
bulbulinx <bulbul.m@inheritx.com>
2026-03-12 19:45:04 +0530

    fixed version issue

--- apps/patient-platform/patient-api/alembic/versions/20260309_0005_move_severity_to_symptom_details.py
+++ apps/patient-platform/patient-api/alembic/versions/20260309_0005_move_severity_to_symptom_details.py
@@ -18,7 +18,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "20260309_0005"
-down_revision: Union[str, None] = "20260308_0004"
+down_revision: Union[str, None] = "20260226_0003"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
