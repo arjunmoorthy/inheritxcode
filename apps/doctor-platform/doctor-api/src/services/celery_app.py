2875c2a27b01b7ac4d9e52eb5d303e58dba8530c
bulbulinx <bulbul.m@inheritx.com>
2026-04-30 16:35:12 +0530

    changed time

--- apps/doctor-platform/doctor-api/src/services/celery_app.py
+++ apps/doctor-platform/doctor-api/src/services/celery_app.py
@@ -12,7 +12,7 @@ celery_app.conf.task_routes = {
 celery_app.conf.beat_schedule = {
     "run-fax-job-every-minute": {
         "task": "run_scheduler_task",
-        "schedule": crontab(minute="*"),
+        "schedule": crontab(hour="9", minute="0"),  # Every 5 minutes for testing, change to crontab(hour=0, minute=0) for daily at midnight
         "options": {"queue": "doctor_queue"},
     }
 }
